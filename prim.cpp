7744d1d59b25e889ba977f06847f41b931a4458a
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2023-08-03 21:38:55 +0530

    Optimized and removed redundancies

--- prim.cpp
+++ prim.cpp
@@ -13,27 +13,15 @@ int G[V][V] = {
   {0, 42, 66, 31, 0}};
 
 int main() {
-  int no_edge;
-  int selected[V];
-
+  int no_edge = 0, selected[V];
   memset(selected, false, sizeof(selected));
-  no_edge = 0;
-
   selected[0] = true;
-
-  int x; 
-  int y;  
-
-  cout << "Edge"
-     << " : "
-     << "Weight";
-  cout << endl;
+  int x, y;  
+  cout << "Edge" << " : " << "Weight" << endl;
   while (no_edge < V - 1) {
-
     int min = INF;
     x = 0;
     y = 0;
-
     for (int i = 0; i < V; i++) {
       if (selected[i]) {
         for (int j = 0; j < V; j++) {
@@ -47,11 +35,9 @@ int main() {
         }
       }
     }
-    cout << x << " - " << y << " :  " << G[x][y];
-    cout << endl;
+    cout << x << " - " << y << " :  " << G[x][y] << endl;
     selected[y] = true;
     no_edge++;
   }
-
   return 0;
 }
